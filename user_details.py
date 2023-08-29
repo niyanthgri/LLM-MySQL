019b8bde40d1c831455c91ce418035c11985edc5
niyanthgri <140157007+niyanthgri@users.noreply.github.com>
2023-08-29 10:00:37 +0530

    Update user_details.py

--- user_details.py
+++ user_details.py
@@ -1,12 +1,12 @@
-MYSQL_HOST=''
-MYSQL_TABLE=""
+MYSQL_HOST=''    ```localhost```
+MYSQL_TABLE=''    ```<table-name>```
 
 # User details for SQL
-SQL_TYPE=''
-SQL_USER_NAME=''
-SQL_PASSWORD=''
-SQL_HOST_URL=''
-SQL_PORT=''
-SQL_DB=''
+SQL_TYPE=''      ```mysql+pymysql```
+SQL_USER_NAME=''    ```<Your MySQL User_name>```
+SQL_PASSWORD=''    ```<Your MySQL Password>```
+SQL_HOST_URL=''    ```<Your MySQL Host URL>```
+SQL_PORT=''        ```<Your MySQL Port>```
+SQL_DB=''          ```<The Database in which the table exists>```
 
 PROD_SPEC_FOLDER = './prod_spec'
